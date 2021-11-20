7ceb48372aaf374e6441238a8328fb5b567cbf14
Mark-dot974 <72488107+Mark-dot974@users.noreply.github.com>
2021-11-20 13:56:54 +0800

    Delete swuFind_pageQuery directory

--- swuFind_pageQuery/src/main/java/com/example/swufind_pagequery/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package com.example.swufind_pagequery;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
