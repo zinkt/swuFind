717054b21dbdcc5333140354bc7931bdd8ad5f6b
zinkt <zinkt@foxmail.com>
2021-11-27 12:21:28 +0800

    submit没办法执行

--- swuFind/src/main/java/swu606309/swuFind/web/servlet/BaseServlet.java
+++ swuFind/src/main/java/swu606309/swuFind/web/servlet/BaseServlet.java
@@ -19,6 +19,8 @@ public class BaseServlet extends HttpServlet {
         String uri = req.getRequestURI();
         //2.获取方法名
         String methodName = uri.substring(uri.lastIndexOf('/') + 1);
+        System.out.println(methodName);
+
         //3.获取方法对象
         try {
             Method method = this.getClass().getMethod(methodName, HttpServletRequest.class, HttpServletResponse.class);
